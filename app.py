d129de18d41b9686d8eb057e415b2d3ae2fd912e
Mario Wüschner <wues@brg-schoren.at>
2024-10-28 20:31:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Mario Wüschner")
 st.write("Hello World")
-text = st.text_input("Geben Sie Ihren Namen ein")
+text = st.text_input("Geben Sie Ihren Namen ein\n")
 st.write("Ihre Antwort:", text)
